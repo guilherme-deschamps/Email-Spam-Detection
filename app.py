0d4ac99108e9ff595fdad0f9a9caffbf19a6f3f3
guilherme-deschamps <guilhermerd6@hotmail.com>
2023-10-16 20:16:02 -0300

    Flask application now has an end-point to predict a received message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from utils import convert_message_to_token
+from utils import convert_message_to_token, predict
 
 app = Flask(__name__)
 
@@ -18,6 +18,23 @@ def convert_message():
 
     # Return the converted message as JSON response
     return jsonify({'converted_message': tokenized_message})
+
+@app.route('/predict', methods=['POST'])
+def predict_message():
+    # Get the message from the request JSON data
+    data = request.get_json()
+    if 'message' not in data:
+        return jsonify({'error': 'Message not provided in JSON data'}), 400
+
+    # Retrieve the message from the JSON data
+    message = data['message']
+
+    # Use the convert_message_to_token function
+    prediction = predict(message)
+
+    # Return the converted message as JSON response
+    response = 'Message is not a spam' if int(prediction) == 0 else 'Message is a spam'
+    return jsonify({'prediction': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
